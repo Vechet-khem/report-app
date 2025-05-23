db6786c17e81fb6cf32e692c80f2053c5ecf1857
Vechet-khem <vechetkhem21@gmail.com>
2025-05-23 09:15:39 +0700

    rename func LandingPage to Page

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image"
 
-export default function LandingPage() {
+export default function Page() {
     return (
         <div className="bg-background">
             <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
